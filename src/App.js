edadae7105a23d312a6cb5162a7b4119132bb6f1
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-11-17 00:28:19 +0700

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import { Greet2 } from './components/Greet2';
+import Welcome from './components/Welcome';
+
+const nickname = 'BelL NaJa';
+
+const user = {
+  firstName : 'TinnakorN',
+  lastName  : 'ChoompeE'
+};
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> {formatName(user)} </p>
+        <p> { nickname } </p>
+        <Greet/>
+        <Greet2/>
+        <Welcome/>
       </header>
     </div>
   );
---
+++ src/components/Greet2.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Greet2 = () => <h1> Hello Tinnakorn2 </h1>
