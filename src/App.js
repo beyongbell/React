72b59a924298b7da143857c909cd7df2a75443e8
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-11-17 06:15:05 +0700

    State React

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Props from './components/Props';
 import PropComponent from './components/PropsComponent';
+import Message from './components/Message';
+
+
 const nickname = 'BelL NaJa';
 
 const user = {
@@ -29,10 +32,9 @@ function App() {
         <Greet2/>
         <Welcome/>
         <Hello/>
-        <Props name="Prop Tinnakorn">
-          <p> This is Children</p>
-        </Props>
+        <Props name="Prop Tinnakorn"> <p> This is Children</p> </Props>
         <PropComponent name="Props Component Bell"/>
+        <Message/>
       </header>
     </div>
   );
